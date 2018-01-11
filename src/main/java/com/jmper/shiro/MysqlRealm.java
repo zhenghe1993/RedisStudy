9b68ecc33662cf956bbe4d61c4cc21ae015fa06b
zhengheming <1143462250@qq.com>
2018-01-12 07:42:24 +0800

    spring-shiro

--- src/main/java/com/jmper/shiro/MysqlRealm.java
+++ src/main/java/com/jmper/shiro/MysqlRealm.java
@@ -1,7 +1,10 @@
 package com.jmper.shiro;
 
 import org.apache.shiro.authc.*;
+import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthenticatingRealm;
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.util.SimpleByteSource;
 
 /**
  * @author 郑和明
@@ -24,8 +27,14 @@ public class MysqlRealm extends AuthenticatingRealm {
         }
 
         Object principle = userName;
-        Object credentials = "123456";
+        Object credentials = "fc1709d0a95a6be30bc5926fdb7f22f4";
         String realName = getName();
-        return new SimpleAuthenticationInfo(principle, credentials, realName);
+        return new SimpleAuthenticationInfo(principle, credentials, ByteSource.Util.bytes(userName), realName);
+    }
+
+
+    public static void main(String[] args) {
+        Object res = new SimpleHash("MD5", "123456", ByteSource.Util.bytes("admin"), 1024);
+        System.out.println(res);
     }
 }
